9fa87b829204177fc235e8bfd08117d719f1d569
朱文赵 <zwz@servyou.com.cn>
2018-01-08 19:57:50 +0800

    go

---
+++ src/main/java/com/ximo/refactoringinaction/RefactoringInActionApplication.java
@@ -0,0 +1,12 @@
+package com.ximo.refactoringinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactoringInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefactoringInActionApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ximo/refactoringinaction/RefactoringInActionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ximo.refactoringinaction;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RefactoringInActionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
